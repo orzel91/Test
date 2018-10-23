16d7e47224d2c3b359d8191503e85d6842f22ffd
Jarek <orel91@o2.pl>
2018-10-23 20:18:51 +0200

    Add new module draft - system

---
+++ stm32_blink_led/test/system/test_system.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "system.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_system_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement system");
+}
